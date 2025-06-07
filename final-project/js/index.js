802564fd0ac8faec374a1c0c26a8a8c6cde2d91e
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-06-07 07:24:47 +0200

    add default object for the timeline

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -1,14 +1,13 @@
 import { controlTimeline } from "./controls.js"
 
 const initAnimations = () => {
-  const tl = gsap.timeline({ paused: true })
-  tl.from("h2", { x: 80, opacity: 0 })
-    .from("h3", { x: -80, opacity: 0 }, "<")
-    .from("p", { y: 30, opacity: 0 })
-    .from(".myButton", { y: 50, opacity: 0 })
+  const tl = gsap.timeline({ paused: true, defaults: { opacity: 0 } })
+  tl.from("h2", { x: 80 })
+    .from("h3", { x: -80 }, "<")
+    .from("p", { y: 30 })
+    .from(".myButton", { y: 50 })
     .from("#items g", {
       transformOrigin: "50% 50%",
-      opacity: 0,
       scale: 0,
       stagger: 0.1,
     })
