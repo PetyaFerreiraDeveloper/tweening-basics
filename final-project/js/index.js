8c0e987b06afbf513cc131340687abbdb63d64c1
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2025-06-07 08:04:18 +0200

    tweek timing

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -2,16 +2,24 @@ import { controlTimeline } from "./controls.js"
 gsap.registerPlugin(GSDevTools)
 
 const initAnimations = () => {
-  const tl = gsap.timeline({ paused: true, defaults: { opacity: 0 } })
-  tl.from("h2", { x: 80 })
-    .from("h3", { x: -80 }, "<")
-    .from("p", { y: 30 })
-    .from(".myButton", { y: 50 })
-    .from("#items g", {
-      transformOrigin: "50% 50%",
-      scale: 0,
-      stagger: 0.1,
-    })
+  const tl = gsap.timeline({
+    paused: true,
+    defaults: { opacity: 0, ease: "back" },
+  })
+  tl.from(".demo", { ease: "linear" })
+    .from("h2", { x: 80, duration: 1 })
+    .from("h3", { x: -80, duration: 1 }, "<")
+    .from("p", { y: 30 }, "-=0.2")
+    .from(".myButton", { y: 50 }, "-=0.4")
+    .from(
+      "#items g",
+      {
+        transformOrigin: "50% 50%",
+        scale: 0,
+        stagger: 0.1,
+      },
+      "-=0.2"
+    )
 
   controlTimeline(tl)
   GSDevTools.create({ animation: tl })
