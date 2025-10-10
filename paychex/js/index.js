6379b9f4a18b4c76162bbc19737e624a49883bc2
Petya Naydenova Ferreira <pn@abtion.com>
2025-10-10 12:36:10 +0200

    create animation and basic styles and layout

--- paychex/js/index.js
+++ paychex/js/index.js
@@ -1,1 +1,20 @@
-console.log("Paychex JS loaded");
+const initAnimations = () => {
+
+  const tl = gsap.timeline({ defaults: { duration: 2, ease: "power2.out" } })
+
+  tl.from('.text', {
+    opacity: 0,
+    x: 50,
+  })
+  .to(
+    ".moving-box", {
+    x: "-100%",
+  }, "<")
+    .from(
+    '.image', {
+      x: "-100%",
+    }, "<") 
+  
+}
+
+document.addEventListener("DOMContentLoaded", initAnimations)
