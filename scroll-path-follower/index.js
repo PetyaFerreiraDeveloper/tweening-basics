f81b1f68b3ba59a6256aee1894005e5dd8337702
Petya Naydenova Ferreira <pn@abtion.com>
2025-10-20 08:50:23 +0200

    scroll driver path follower

---
+++ scroll-path-follower/index.js
@@ -0,0 +1,28 @@
+gsap.registerPlugin(ScrollTrigger, MotionPathPlugin, DrawSVGPlugin);
+
+gsap.defaults({ease: 'none'})
+
+const main = gsap
+  .timeline({
+    scrollTrigger: {
+      scroller: ".scroll-window",
+      trigger: ".svg",
+      scrub: true,
+      start: "top top",
+      end: 'max',
+    },
+  })
+  // .from(".theLine", { drawSVG: 0, duration: 4 }, 0)
+  .to(
+    ".ball01",
+    {
+      motionPath: {
+        path: ".theLine",
+        align: ".theLine",
+        alignOrigin: [0.5, 0.5],
+      },
+      duration: 4,
+    },
+    0
+  );
+
